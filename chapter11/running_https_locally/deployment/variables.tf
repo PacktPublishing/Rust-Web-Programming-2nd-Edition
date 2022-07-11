a8a608f1f5f28d84cee46543b3ffdae6f64ad166
maxwell flitton <maxwellflitton@maxwells-MacBook-Pro.local>
2022-07-11 01:40:01 +0100

    chapter 11 done

---
+++ chapter11/running_https_locally/deployment/variables.tf
@@ -0,0 +1,12 @@
+
+
+variable "db_password" {
+    description = "The password for the database"
+    default = "password"
+}
+
+
+variable "db_username" {
+    description = "The username for the database"
+    default = "username"
+}
