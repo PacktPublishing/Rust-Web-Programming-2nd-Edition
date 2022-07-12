af7ea9fae9b3fe468d9f9af6adeab1876812bacb
maxwell flitton <maxwellflitton@maxwells-MacBook-Pro.local>
2022-07-12 22:50:06 +0100

    adding rocker

---
+++ chapter12/setting_up_the_server/rocket_app/src/main.rs
@@ -0,0 +1,22 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/bye/<name>/<age>")]
+fn bye(name: String, age: u8) -> String {
+    format!("Goodbye, {} year old named {}!", age,
+    name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, hello, bye])
+}
